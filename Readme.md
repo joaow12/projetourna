61882f8adfec4969105f387ef3f93b746f34ebe6
João Victor <jujotasurf@gmail.com>
2021-09-05 13:20:14 -0300

    Projeto Urna

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Projeto de uma urna feito usando HTML/CSS e Javascript.
+
+Feito junto ao video "3 Projetos p/ Portfólio em Javascript para Iniciantes" do canal Bonieky Lacerda
+
+Link: https://www.youtube.com/watch?v=hF_VMWnsY00&t=11639s
---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'blank-profile-picture-973460__480.webp', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                name: 'Beltrano de Tal',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'blank-profile-picture-973460__480.webp', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'blank-profile-picture-973460__480.webp', legenda: 'Prefeito'},
+                    {url: 'blank-profile-picture-973460__480.webp', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'blank-profile-picture-973460__480.webp', legenda: 'Prefeito'},
+                    {url: 'blank-profile-picture-973460__480.webp', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
